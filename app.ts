0aad975ad1534779e527fbd3e1a6492c0ec15fba
Rafael Coutinho <rafacout@gmail.com>
2018-11-28 11:00:25 -0200

    Initial project of TypeScript and some examples with types

---
+++ app.ts
@@ -0,0 +1,14 @@
+let message: string = "Hello world"
+console.log(message)
+
+let episode: number = 4
+console.log(episode)
+episode = episode + 1
+console.log(episode)
+
+
+let favoriteDroid
+favoriteDroid = "BB-8" //evitar nao definir variaveis sem tipo
+favoriteDroid = 10 
+
+console.log(favoriteDroid)
---
+++ dist/app.js
@@ -0,0 +1,10 @@
+var message = "Hello world";
+console.log(message);
+var episode = 4;
+console.log(episode);
+episode = episode + 1;
+console.log(episode);
+var favoriteDroid;
+favoriteDroid = "BB-8"; //evitar nao definir variaveis sem tipo
+favoriteDroid = 10;
+console.log(favoriteDroid);
